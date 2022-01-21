43f80fabb9c5c378d963ee6fe190bf39ce9c0e86
Lasse Herzog <lasse@herzoeglich.de>
2022-01-21 11:52:14 +0100

    added collision detection to pacmans movement

--- pacman/__init__.py
+++ pacman/__init__.py
@@ -5,6 +5,39 @@ import pygame
 from pygame.locals import *
 
 
+class Pacman(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = load_image("pacman_01.png")
+        self.rect = self.image.get_rect(center=(113, 188))
+        self.mask = pygame.mask.Mask((16, 16), True)
+
+        self.speed = (0, 0)
+        self.old_speed = (0, 0)
+
+    def update(self):
+        self._move()
+
+    def _move(self):
+        if will_collide_with_wall(self, self.speed):
+            if will_collide_with_wall(self, self.old_speed):
+                return
+            else:
+                self.rect = self.rect.move(self.old_speed)
+        else:
+            self.rect = self.rect.move(self.speed)
+            self.old_speed = self.speed
+
+
+def load_image(i):
+    return pygame.image.load(os.path.join("data", i))
+
+
+def will_collide_with_wall(self, speed):
+    return labyrinth_collision_mask.overlap(self.mask, self.rect.move(speed).topleft)
+
+
 def input(events):
     for event in events:
         if event.type == QUIT:
@@ -20,34 +53,17 @@ def input(events):
                 pacman.speed = (0, 1)
 
 
-def load_image(i):
-    image = pygame.image.load(os.path.join("data", i))
-    return image, image.get_rect()
-
-
-class Pacman(pygame.sprite.Sprite):
-    old_speed = (0, 0)
-    speed = (0, 0)
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image("pacman_01.png")
-
-    def update(self):
-        self._move()
-
-    def _move(self):
-        self.rect = self.rect.move(self.speed)
-
-
 if __name__ == '__main__':
     pygame.init()
 
     window = pygame.display.set_mode((224, 248))
+
     pygame.display.set_caption('Pacman')
     screen = pygame.display.get_surface()
 
-    terrain = pygame.image.load(os.path.join("data", "terrain.png"))
+    labyrinth = load_image("labyrinth.png")
+    labyrinth_collision_mask = pygame.mask.from_surface(load_image("labyrinth_collision_map.png"))
+
     pacman = Pacman()
 
     all_sprites = pygame.sprite.RenderPlain(pacman)
@@ -58,6 +74,9 @@ if __name__ == '__main__':
         clock.tick(60)
         input(pygame.event.get())
         all_sprites.update()
-        screen.blit(terrain, (0, 0))
+
+        screen.fill([0, 0, 0])
+        screen.blit(labyrinth, (0, 0))
+
         all_sprites.draw(screen)
         pygame.display.flip()
