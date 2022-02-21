08341635213c07d82b74775e9a083ed726d47728
Lasse Herzog <lasse.herzog@exxcellent.de>
2022-02-21 22:47:12 +0100

    - refactored main menu - added controller support & sound

--- main.py
+++ main.py
@@ -1,62 +1,63 @@
 import os
+from enum import IntEnum
 
 import pygame
-from pygame.locals import *
 
-from pong.main import main_menu as pong_menu
+import pacman.main
+import pong.main
 import snake.main
 import space_invaders.main
-import pacman.main
 
 
 def load_asset(asset):
     return os.path.join('assets', asset)
 
 
+# Game Fonts
+HEADER_FONT = load_asset('ArcadeClassic-ov2x.ttf')
+FONT = load_asset('Pixeled.ttf')
+
+# Game Resolution
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 600
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (50, 50, 50)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GRAY_B = (25, 25, 75)
+DARK_BLUE = (0, 0, 152)
+
 # Game Initialization
 pygame.init()
 
-# Joystick Initialization
-pygame.joystick.init()
-
 # Center the Game
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-
-# Game Resolution
-screen_width = 1024
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 # Text Renderer
-def text_format(message, textFont, textSize, textColor):
-    newFont = pygame.font.Font(textFont, textSize)
-    newText = newFont.render(message, 0, textColor)
+def text_format(message, font_type, font_size, color):
+    font = pygame.freetype.Font(font_type, font_size)
 
-    return newText
+    return font.render(message, color)
 
-
-# Colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray = (50, 50, 50)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-gray_b = (25, 25, 75)
-dark_blue = (0, 0, 152)
-
-# Game Fonts
-font = load_asset('Pixeled.ttf')
-header_font = load_asset('ArcadeClassic-ov2x.ttf')
 
 # Sounds
-select_sound = pygame.mixer.Sound("assets/select.wav")
+change_selection_sound = pygame.mixer.Sound(load_asset('choose.wav'))
+choose_selection_sound = pygame.mixer.Sound(load_asset('choose_this.wav'))
+go_back_sound = pygame.mixer.Sound(load_asset('go_back.wav'))
+
+pygame.mixer.music.load(load_asset('main_menu.wav'))
+pygame.mixer.music.play(-1)
 
 # Game Framerate
 clock = pygame.time.Clock()
-FPS = 30
+FPS = 15
 
 # Main Menu
 if pygame.joystick.get_count() > 0:
@@ -64,212 +65,222 @@ if pygame.joystick.get_count() > 0:
     joystick.init()
 
 
+class MainMenuOptions(IntEnum):
+    PONG = 0
+    SNAKE = 1
+    PAC_MAN = 2
+    SPACE_INVADERS = 3
+    BACK = 4
+
+
+SHOW_TITLE_EVENT = pygame.USEREVENT + 1
+
+
 def main_menu():
-    menu = True
-    selected = "start"
-    start_time = pygame.time.get_ticks()
-    show_text = 0
+    pygame.display.set_caption("PiG-C Main Menu")
 
-    while menu:
-        for event in pygame.event.get():  # Keyboard Controls
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type == pygame.KEYDOWN:
-                pygame.mixer.Sound.play(select_sound)
-                game_select()
-            elif event.type == JOYBUTTONDOWN:
-                pygame.mixer.Sound.play(select_sound)
-                print("Joystick button pressed.")
-                game_select()
-            elif event.type == pygame.JOYBUTTONDOWN:
-                pygame.mixer.Sound.play(select_sound)
-                print("Joystick button pressed.")
-                game_select()
+    pygame.time.set_timer(SHOW_TITLE_EVENT, 500)
 
-        # Main Menu UI
-        screen.fill(black)
-        title = text_format("RETRO PiG", header_font, 100, white)
-        text_start = text_format("Press any button", font, 20, white)
+    visible = True
 
-        title_rect = title.get_rect()
-        start_rect = text_start.get_rect()
+    title_text, title_rect = text_format("RETRO PiG", HEADER_FONT, 100, WHITE)
+    white_start_text, start_rect = text_format("Press any button", FONT, 20, WHITE)
+    black_start_text, start_rect = text_format("Press any button", FONT, 20, BLACK)
 
-        # Main Menu Text
-        screen.blit(title, (screen_width / 2 - (title_rect[2] / 2), 200))
-        if (show_text < 15):
-            screen.blit(text_start, (screen_width / 2 - (start_rect[2] / 2) + 10, 400))
+    def init_ui():
+        screen.fill(BLACK)
+        screen.blit(title_text, (SCREEN_WIDTH / 2 - (title_rect[2] / 2), 200))
 
-        if (show_text > 30):
-            show_text = 0
-            show_text += 1
-        else:
-            show_text += 1
-        pygame.display.update()
+    init_ui()
+
+    while True:
         clock.tick(FPS)
-        pygame.display.set_caption("PiG-C Main Menu")
+
+        # Keyboard Controls
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.mixer.Sound.play(go_back_sound)
+                pygame.quit()
+            elif event.type == SHOW_TITLE_EVENT:
+                visible = not visible
+                start_text = white_start_text if visible else black_start_text
+
+                screen.blit(start_text, (SCREEN_WIDTH / 2 - (start_rect[2] / 2) + 10, 400))
+            elif event.type == pygame.KEYDOWN or event == pygame.JOYBUTTONDOWN:
+                pygame.mixer.Sound.play(change_selection_sound)
+                game_select()
+                init_ui()
+
+        pygame.display.update()
 
 
 # Game select
 def game_select():
     menu = True
-    selection = ["pong", "Snake", "Pac-Man", "Space Invaders", "back"]
-    s: int = 0
-    selected = selection[s]
+    selection = MainMenuOptions.PONG
     last_select = 0
 
-    while menu:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                pygame.mixer.Sound.play(select_sound)
-                if event.key == pygame.K_UP:
-                    s -= 1
-                    if (s < 0):
-                        s = 4
-                    selected = selection[s]
-                    print(selected)
-                elif event.key == pygame.K_DOWN:
-                    s += 1
-                    if (s > 4):
-                        s = 0
-                    selected = selection[s]
-                    print(selected)
-                if event.key == pygame.K_RETURN:
-                    if selected == "pong":
-                        print("pong Start")
-                        pong_menu()
-                    if selected == "Snake":
-                        print("Snake Start")
-                        snake.main.gameLoop()
-                    if selected == "Pac-Man":
-                        print("Pac-Man Start")
-                        pacman.main.start_level()
-                    if selected == "Space Invaders":
-                        print("Space Invaders Start")
-                        space_invaders.main.main()
-                    if selected == "back":
-                        print("back to menu")
-                        main_menu()
-            if event.type == JOYAXISMOTION:
-                # Joystick Controls
-                axis = [0, 0]
+    title_text, title_text_rect = text_format("GAME SELECT", FONT, 45, WHITE)
 
-                for j in range(2):
-                    axis[j] = joystick.get_axis(j)
+    pong_unselected_text, pong_text_rect = text_format("Pong", FONT, 35, GRAY)
+    pong_selected_text, pong_text_rect = text_format("Pong", FONT, 35, WHITE)
 
-                if round(axis[0]) == 1 and axis[1] == 0 and last_select + 1000 < pygame.time.get_ticks():  # Joystick Up
-                    last_select = pygame.time.get_ticks()
-                    s -= 1
-                    if (s < 0):
-                        s = 4
-                    selected = selection[s]
-                    print(selected)
-                if round(axis[0]) == -1 and axis[
-                    1] == 0 and last_select + 1000 < pygame.time.get_ticks():  # Joystick Down
-                    last_select = pygame.time.get_ticks()
-                    s += 1
-                    if (s > 4):
-                        s = 0
-                    selected = selection[s]
-                    print(selected)
+    snake_unselected_text, snake_text_rect = text_format("Snake", FONT, 35, GRAY)
+    snake_selected_text, snake_text_rect = text_format("Snake", FONT, 35, GREEN)
 
-            if event.type == JOYBUTTONDOWN:
-                if selected == "pong":
-                    print("pong Start")
-                if selected == "Snake":
-                    print("Snake Start")
-                    snake.main.gameLoop()
-                if selected == "Pac-Man":
-                    print("Pac-Man Start")
-                if selected == "Space Invaders":
-                    print("Space Invaders Start")
-                if selected == "back":
-                    print("back to menu")
-                    main_menu()
+    pacman_unselected_text, pacman_text_rect = text_format("Pac-Man", FONT, 35, GRAY)
+    pacman_selected_text, pacman_text_rect = text_format("Pac-Man", FONT, 35, YELLOW)
 
-        # Game demo Rectangles
-        # pong
-        paddle = pygame.Rect(screen_width - 31, screen_height / 2 - 15, 10, 120)
-        paddle_2 = pygame.Rect(32, screen_height / 2 - 125, 10, 120)
-        ball = pygame.Rect(screen_width - 181, screen_height / 2 - 25, 15, 15)
-        # Snake
-        snake_icon = pygame.Rect(55, screen_height / 2 - 110, 50, 10)
-        apple = pygame.Rect(screen_width - 181, screen_height - 200, 20, 20)
-        # Pac-Man
-        pacman_icon = pygame.image.load(load_asset('pm_pacman.png'))
-        superpill = pygame.Rect(822, screen_height / 2 - 105, 15, 15)
-        # Space Invaders
-        crab = pygame.image.load(load_asset('si_crab.png'))
-        octopus = pygame.image.load(load_asset('si_octopus.png'))
-        player = pygame.image.load(load_asset('si_player.png'))
+    si_unselected_text, si_text_rect = text_format("Space Invaders", FONT, 35, GRAY)
+    si_selected_text, si_text_rect = text_format("Space Invaders", FONT, 35, BLUE)
 
-        # Select Menu UI
-        screen.fill(black)
-        title = text_format("GAME SELECT", font, 45, white)
-        if selected == "pong":
-            text_pong = text_format("pong", font, 35, white)
-            pygame.draw.rect(screen, white, paddle)
-            pygame.draw.rect(screen, white, paddle_2)
-            pygame.draw.ellipse(screen, white, ball)
-        else:
-            text_pong = text_format("pong", font, 35, gray)
+    back_unselected_text, back_text_rect = text_format("back to menu", FONT, 35, GRAY)
+    back_selected_text, back_text_rect = text_format("back to menu", FONT, 35, GRAY_B)
 
-        if selected == "Snake":
-            text_snake = text_format("Snake", font, 35, green)
-            i = 0
-            while (i < 121):
-                snake_icon = pygame.Rect(55 + i, screen_height / 2 - 100, 15, 15)
-                pygame.draw.ellipse(screen, green, snake_icon)
-                i += 15
-            pygame.draw.ellipse(screen, red, apple)
-        else:
-            text_snake = text_format("Snake", font, 35, gray)
-        if selected == "Pac-Man":
-            text_pacman = text_format("Pac-Man", font, 35, yellow)
-            i = 0
-            while (i < 197):
-                pills_1 = pygame.Rect(0 + i, screen_height / 2 - 100, 5, 5)
-                pills_2 = pygame.Rect(1024 - i, screen_height / 2 - 100, 5, 5)
-                pygame.draw.ellipse(screen, white, pills_1)
-                pygame.draw.ellipse(screen, white, pills_2)
-                i += 15
-            screen.blit(pacman_icon, (176, screen_height / 2 - 115))
-            pygame.draw.ellipse(screen, white, superpill)
-        else:
-            text_pacman = text_format("Pac-Man", font, 35, gray)
-        if selected == "Space Invaders":
-            text_si = text_format("Space Invaders", font, 35, blue)
-            screen.blit(crab, (85, screen_height / 2 - 125))
-            screen.blit(octopus, (screen_width - 161, screen_height - 125))
-        else:
-            text_si = text_format("Space Invaders", font, 35, gray)
-        if selected == "back":
-            text_back = text_format("back to menu", font, 35, gray_b)
-        else:
-            text_back = text_format("back to menu", font, 35, gray)
-        # Text Rectangles
-        title_rect = title.get_rect()
-        pong_rect = text_pong.get_rect()
-        snake_rect = text_snake.get_rect()
-        pacman_rect = text_pacman.get_rect()
-        si_rect = text_si.get_rect()
-        back_rect = text_back.get_rect()
+    # Game demo Rectangles
+    # Pong
+    paddle = pygame.Rect(SCREEN_WIDTH - 31, SCREEN_HEIGHT / 2 - 15, 10, 120)
+    paddle_2 = pygame.Rect(32, SCREEN_HEIGHT / 2 - 125, 10, 120)
+    ball = pygame.Rect(SCREEN_WIDTH - 181, SCREEN_HEIGHT / 2 - 25, 15, 15)
+
+    # Snake
+    # snake_icon = pygame.Rect(55, SCREEN_HEIGHT / 2 - 110, 50, 10)
+    apple = pygame.Rect(SCREEN_WIDTH - 181, SCREEN_HEIGHT - 200, 20, 20)
+
+    # Pac-Man
+    pacman_icon = pygame.image.load(load_asset('pm_pacman.png'))
+    superpill = pygame.Rect(822, SCREEN_HEIGHT / 2 - 105, 15, 15)
+
+    # Space Invaders
+    crab = pygame.image.load(load_asset('si_crab.png'))
+    octopus = pygame.image.load(load_asset('si_octopus.png'))
+
+    # player = pygame.image.load(load_asset('si_player.png'))
+
+    def move_up_option():
+        nonlocal selection
+        pygame.mixer.Sound.play(change_selection_sound)
+        selection = selection - 1 if selection - 1 >= 0 else MainMenuOptions.BACK
+        redraw()
+
+    def move_down_option():
+        nonlocal selection
+        pygame.mixer.Sound.play(change_selection_sound)
+        selection = selection + 1 if selection + 1 <= 4 else MainMenuOptions.PONG
+        redraw()
+
+    def select_option():
+        pygame.mixer.Sound.play(choose_selection_sound)
+
+        match selection:
+            case MainMenuOptions.PONG:
+                pygame.mixer.music.fadeout(3)
+                pong.main.main_menu()
+            case MainMenuOptions.SNAKE:
+                pygame.mixer.music.fadeout(3)
+                snake.main.gameLoop()
+            case MainMenuOptions.PAC_MAN:
+                pygame.mixer.music.fadeout(3)
+                pacman.main.start_level()
+            case MainMenuOptions.SPACE_INVADERS:
+                pygame.mixer.music.fadeout(3)
+                space_invaders.main.main()
+            case MainMenuOptions.BACK:
+                return True
+
+    def redraw():
+        pong_text = pong_unselected_text
+        snake_text = snake_unselected_text
+        pacman_text = pacman_unselected_text
+        si_text = si_unselected_text
+        back_text = back_unselected_text
+
+        screen.fill(BLACK)
+
+        match selection:
+            case MainMenuOptions.PONG:
+                pong_text = pong_selected_text
+
+                pygame.draw.rect(screen, WHITE, paddle)
+                pygame.draw.rect(screen, WHITE, paddle_2)
+                pygame.draw.ellipse(screen, WHITE, ball)
+            case MainMenuOptions.SNAKE:
+                snake_text = snake_selected_text
+
+                i = 0
+                while i < 121:
+                    snake_icon = pygame.Rect(55 + i, SCREEN_HEIGHT / 2 - 100, 15, 15)
+                    pygame.draw.ellipse(screen, GREEN, snake_icon)
+                    i += 15
+                pygame.draw.ellipse(screen, RED, apple)
+            case MainMenuOptions.PAC_MAN:
+                pacman_text = pacman_selected_text
+
+                i = 0
+                while i < 197:
+                    pills_1 = pygame.Rect(0 + i, SCREEN_HEIGHT / 2 - 100, 5, 5)
+                    pills_2 = pygame.Rect(1024 - i, SCREEN_HEIGHT / 2 - 100, 5, 5)
+                    pygame.draw.ellipse(screen, WHITE, pills_1)
+                    pygame.draw.ellipse(screen, WHITE, pills_2)
+                    i += 15
+                screen.blit(pacman_icon, (176, SCREEN_HEIGHT / 2 - 115))
+                pygame.draw.ellipse(screen, WHITE, superpill)
+            case MainMenuOptions.SPACE_INVADERS:
+                si_text = si_selected_text
+
+                screen.blit(crab, (85, SCREEN_HEIGHT / 2 - 125))
+                screen.blit(octopus, (SCREEN_WIDTH - 161, SCREEN_HEIGHT - 125))
+            case MainMenuOptions.BACK:
+                back_text = back_selected_text
 
         # Main Menu Text
-        screen.blit(title, (screen_width / 2 - (title_rect[2] / 2), 50))
-        screen.blit(text_pong, (screen_width / 2 - (pong_rect[2] / 2), 200))
-        screen.blit(text_snake, (screen_width / 2 - (snake_rect[2] / 2), 260))
-        screen.blit(text_pacman, (screen_width / 2 - (pacman_rect[2] / 2), 320))
-        screen.blit(text_si, (screen_width / 2 - (si_rect[2] / 2), 380))
-        screen.blit(text_back, (screen_width / 2 - (back_rect[2] / 2), 440))
+        screen.blit(title_text, (SCREEN_WIDTH / 2 - (title_text_rect[2] / 2), 50))
+        screen.blit(pong_text, (SCREEN_WIDTH / 2 - (pong_text_rect[2] / 2), 200))
+        screen.blit(snake_text, (SCREEN_WIDTH / 2 - (snake_text_rect[2] / 2), 260))
+        screen.blit(pacman_text, (SCREEN_WIDTH / 2 - (pacman_text_rect[2] / 2), 320))
+        screen.blit(si_text, (SCREEN_WIDTH / 2 - (si_text_rect[2] / 2), 380))
+        screen.blit(back_text, (SCREEN_WIDTH / 2 - (back_text_rect[2] / 2), 440))
+
         pygame.display.update()
+
+    redraw()
+
+    while menu:
         clock.tick(FPS)
-        pygame.display.set_caption("PiG-C pong Main Menu")
+
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    pygame.mixer.Sound.play(go_back_sound)
+
+                    pygame.quit()
+                case pygame.KEYDOWN:
+                    match event.key:
+                        case pygame.K_UP:
+                            move_up_option()
+                        case pygame.K_DOWN:
+                            move_down_option()
+                        case pygame.K_RETURN:
+                            if select_option():
+                                pygame.mixer.Sound.play(go_back_sound)
+
+                                return
+                case pygame.JOYAXISMOTION:
+                    if last_select + 500 >= pygame.time.get_ticks():
+                        break
+
+                    axis = [joystick.get_axis(i) for i in range(2)]
+                    last_select = pygame.time.get_ticks()
+
+                    if round(axis[0]) == 1 and round(axis[1]) == 0:
+                        move_up_option()
+                    elif round(axis[0]) == -1 and round(axis[1]) == 0:
+                        move_down_option()
+                case pygame.JOYBUTTONDOWN:
+                    if select_option():
+                        pygame.mixer.Sound.play(go_back_sound)
+
+                        return
 
 
 main_menu()
-pygame.quit()
-quit()
