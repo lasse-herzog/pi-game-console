21f31a9fa711aa69f52f11c84b2c8bed7a9dee76
Lasse Herzog <lasse.herzog@exxcellent.de>
2022-02-22 23:49:28 +0100

    - started docs for Pacman

--- pacman/utils.py
+++ pacman/utils.py
@@ -16,9 +16,6 @@ GHOST_FRIGHT_SPEED = [80, 88, 96, 152]
 
 
 class Directions(Enum):
-    def __eq__(self, other):
-        return self.value == other.value
-
     UP = (0, -1)
     LEFT = (-1, 0)
     DOWN = (0, 1)
