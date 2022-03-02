774c7811495a750f74992f085fd44ec2e62caad4
lasse-herzog <PrometheusSSE@outlook.com>
2022-03-02 15:24:03 +0100

    Implement Navigation with Vue Router 4

--- website/src/main.js
+++ website/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import App from './App.vue';
+import Start from './components/StartPage.vue';
+import Test from './components/Test.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Start },
+  { path: '/test', component: Test },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');
