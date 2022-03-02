c1689d24d3c5d05d64d5b6b7ede1e53869951836
lasse-herzog <PrometheusSSE@outlook.com>
2022-03-02 15:24:03 +0100

    Implement Navigation with Vue Router 4

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,11 +1,5 @@
-<script setup>
-import StartPage from './components/StartPage.vue';
-</script>
-
 <template>
-  <div>
-    <StartPage />
-  </div>
+  <router-view />
 </template>
 
 <style>
@@ -18,8 +12,5 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
