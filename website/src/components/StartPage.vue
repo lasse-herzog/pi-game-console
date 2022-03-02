d5a8d58adafbeaffa03bd3a813dde954613062f6
Prometheus <herzogl.tin21@student.dhbw-heidenheim.de>
2022-03-02 15:24:03 +0100

    Implement Navigation with Vue Router 4

--- website/src/components/StartPage.vue
+++ website/src/components/StartPage.vue
@@ -6,6 +6,7 @@
 import * as THREE from 'three';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default {
   data() {
@@ -16,19 +17,26 @@ export default {
   },
   methods: {
     animation(time) {
+      this.controls.update();
+
       this.renderer.render(this.scene, this.camera);
     },
     init() {
+      this.arcades = [];
       this.container = this.$refs.container;
 
       this.camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth / window.innerHeight,
-        0.25,
-        20
+        5,
+        50
       );
+      this.camera.position.set(13, 2, 20);
 
       this.scene = new THREE.Scene();
+
+      this.raycaster = new THREE.Raycaster();
+      this.mouse = new THREE.Vector2();
 
       const loader = new GLTFLoader();
 
@@ -49,9 +57,32 @@ export default {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.renderer.setAnimationLoop(this.animation);
       this.container.appendChild(this.renderer.domElement);
+
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      // this.controls.enablePan = false;
+      this.controls.enableDamping = true;
+
+      this.renderer.domElement.addEventListener('click', this.onClick, false);
+    },
+    initInteractiveObjects(child) {
+      if (/^Arcade/.test(child.name)) {
+        this.arcades.push(child);
+      }
     },
     loadGltf(gltf) {
+      gltf.scene.traverse(this.initInteractiveObjects);
       this.scene.add(gltf.scene);
+    },
+    onClick() {
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      this.raycaster.setFromCamera(this.mouse, this.camera);
+      var intersectsArcade = this.raycaster.intersectObjects(this.arcades);
+
+      if (intersectsArcade.length > 0) {
+        this.$router.push('test');
+      }
     },
   },
 };
