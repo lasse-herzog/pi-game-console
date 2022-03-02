1c9a3777a9c49dcfbe47fe5a7c77411d25e42483
Prometheus <PrometheusSSE@outlook.com>
2022-03-02 11:48:22 +0100

    Added ThreeJS Boilerplate

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import StartPage from './components/StartPage.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Moin" />
+  <div>
+    <StartPage />
+  </div>
 </template>
 
 <style>
