ade4fdcf68ba4d58816d7d031ecd3bdb25b41e46
Jonas Wiedenmann <jwiedenmann@exone.de>
2022-03-04 09:21:13 +0100

    structure prepared

---
+++ website/src/components/NavigationBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({})
+</script>
+
+<template>
+<div class="navbar">
+  <img src="../assets/logo.png"/>
+</div>
+</template>
+
+<style scoped>
+img{
+  height: 10rem;
+}
+.navbar{
+
+}
+</style>
