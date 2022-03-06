af9c9c18d3ee0add557ef4a4fc386202ac699fa9
Jonas Wiedenmann <jwiedenmann@exone.de>
2022-03-06 17:38:27 +0100

    wip

--- website/src/App.vue
+++ website/src/App.vue
@@ -2,20 +2,35 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import GameConsole from './components/GameConsole.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Moin" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Moin" /> -->
+  <GameConsole />
 </template>
 
 <style>
+@font-face {
+  font-family: "Liberty";
+  src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/907368/liberty.otf");
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+}
+
+html,
+body {
+  background-color: #000;
+  margin: 0 !important;
+  padding: 0 !important; 
+  scroll-behavior: smooth;
 }
 </style>
