e80f24d941cd6556d1092ccdae22be427ed5a563
Prometheus <PrometheusSSE@outlook.com>
2022-03-09 17:36:16 +0100

    Final Render Version for presentation

--- website/src/main.js
+++ website/src/main.js
@@ -1,12 +1,18 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
+import GameConsole from './components/GameConsole.vue';
+import Games from './components/Games.vue';
 import Start from './components/StartPage.vue';
-import Test from './components/Test.vue';
 
 const routes = [
   { path: '/', component: Start },
-  { path: '/test', component: Test },
+  { path: '/console', component: GameConsole },
+  //{ path: '/datenschutz', component: Datenschutz },
+  //{ path: '/faq', component: FAQ },
+  { path: '/games', component: Games },
+  //{ path: '/impressum', component: Impressum },
+  //{ path: '/shop', component: Shop },
 ];
 
 const router = createRouter({
