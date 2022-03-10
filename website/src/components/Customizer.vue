f345dc4bc21a5d252c79cd78ba803babbabdf9e0
larsjaeger <lars@jgr.one>
2022-03-10 09:19:04 +0100

    adding three.js preview

---
+++ website/src/components/Customizer.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  mounted() {
+    const renderer = this.$refs.renderer;
+    const box = this.$refs.box.mesh;
+    renderer.onBeforeRender(() => {
+      box.rotation.x += 0.01;
+    });
+  },
+};
+</script>
