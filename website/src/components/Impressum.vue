5b70229b7fe47dab0b2115027b134827a14d9ad0
Lasse Herzog <herzogl.tin21@student.dhbw-heidenheim.de>
2022-03-27 15:21:10 +0200

    Made Impressum impressive

--- website/src/components/Impressum.vue
+++ website/src/components/Impressum.vue
@@ -1,96 +1,78 @@
-<script></script>
-
 <template>
-    <body>
-        <div class="text">
-            <h1>Impressum</h1>
+  <div class="container">
+    <div class="text">
+      <h1>Impressum</h1>
 
-            <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-            <p>
-                PYCO AG
-                <br />Musterstra&szlig;e 111
-                <br />Geb&auml;ude 44
-                <br />90210 Musterstadt
-            </p>
+      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+      <p>
+        PYCO AG
+        <br />Musterstra&szlig;e 111 <br />Geb&auml;ude 44 <br />90210
+        Musterstadt
+      </p>
 
-            <p>
-                <strong>Vertreten durch:</strong>
-                <br />Dr. Harry Mustermann
-                <br />Luise Beispiel
-            </p>
+      <p>
+        <strong>Vertreten durch:</strong>
+        <br />Dr. Harry Mustermann <br />Luise Beispiel
+      </p>
 
-            <p>
-                <strong>Vorsitzender des Aufsichtsrats:</strong>
-                <br />Richard Amtmann
-            </p>
+      <p>
+        <strong>Vorsitzender des Aufsichtsrats:</strong>
+        <br />Richard Amtmann
+      </p>
 
-            <h2>Kontakt</h2>
-            <p>
-                Telefon: +49 (0) 123 44 55 66
-                <br />Telefax: +49 (0) 123 44 55 99
-                <br />E-Mail: mustermann@musterfirma.de
-            </p>
+      <h2>Kontakt</h2>
+      <p>
+        Telefon: +49 (0) 123 44 55 66
+        <br />Telefax: +49 (0) 123 44 55 99 <br />E-Mail:
+        mustermann@musterfirma.de
+      </p>
 
-            <h2>Umsatzsteuer-ID</h2>
-            <p>
-                Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:
-                <br />DE999999999
-            </p>
+      <h2>Umsatzsteuer-ID</h2>
+      <p>
+        Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+        Umsatzsteuergesetz:
+        <br />DE999999999
+      </p>
 
-            <h2>Redaktionell verantwortlich</h2>
-            <p>
-                Beate Beispielhaft
-                <br />Musterstra&szlig;e 110
-                <br />90210 Musterstadt
-            </p>
+      <h2>Redaktionell verantwortlich</h2>
+      <p>
+        Beate Beispielhaft
+        <br />Musterstra&szlig;e 110 <br />90210 Musterstadt
+      </p>
 
-            <h2>EU-Streitschlichtung</h2>
-            <p>
-                Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
-                <a
-                    href="https://ec.europa.eu/consumers/odr/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >https://ec.europa.eu/consumers/odr/</a>.
-                <br />Unsere E-Mail-Adresse finden Sie oben im Impressum.
-            </p>
+      <h2>EU-Streitschlichtung</h2>
+      <p>
+        Die Europ&auml;ische Kommission stellt eine Plattform zur
+        Online-Streitbeilegung (OS) bereit:
+        <a
+          href="https://ec.europa.eu/consumers/odr/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >https://ec.europa.eu/consumers/odr/</a
+        >. <br />Unsere E-Mail-Adresse finden Sie oben im Impressum.
+      </p>
 
-            <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
-        </div>
-    </body>
+      <h2>
+        Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+      </h2>
+    </div>
+  </div>
 </template>
 
-
 <style scoped>
-body {
-    width: 100%;
-    height: 100%;
-    background: #282A36;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    box-shadow: 1rem 5px 1rem rgba(0, 0, 0, 0.1);
+.container {
+  background: #282a36;
+  box-shadow: 1rem 5px 1rem rgba(0, 0, 0, 0.1);
 }
 
-.text{
-    color: #F8F8F2;
-    background: #A243C2;
-    padding: 2rem;
-    margin: 3rem;
-    border-radius: 15px;
+.text {
+  color: #f8f8f2;
+  background: #a243c2;
+  padding: 2rem;
+  margin: 3rem;
+  border-radius: 15px;
 }
-
-.text h1 {
-  font-size: 5rem;
-}
-
-.text h2{
-    font-size: 3rem;
-    color: #282A36;
-}
-
-.text p {
-  font-size: 2rem;
+.text h2 {
+  color: #282a36;
 }
 </style>
